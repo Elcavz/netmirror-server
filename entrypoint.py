52d302a247df1ae2c3a37496f28960c146b01baa
Elcavz <89607443+Elcavz@users.noreply.github.com>
2026-06-06 09:16:51 +0800

    update

--- entrypoint.py
+++
@@ -1,7 +0,0 @@
-from workers import WorkerEntrypoint
-import asgi
-from main import app   # Make sure 'app' is the name of your FastAPI() object
-
-class Default(WorkerEntrypoint):
-    async def fetch(self, request):
-        return await asgi.fetch(app, request.js_object, self.env)
